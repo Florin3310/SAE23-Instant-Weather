aa23c9bff60da8d31577dc257cacbc2810548cca
Florin3310 <florinelcatalin18@gmail.com>
2025-05-27 14:43:11 +0200

    Importation des fichiers dans le dépôt Git

---
+++ script.js
@@ -0,0 +1,6 @@
+// Récupération des données
+const Urlmeteo = "https://api.meteo-concept.com/api";
+const Urlgeo = "https://geo.api.gouv.fr/communes";
+const token = "5d1f8c4cbc841dfcb1e53129568d86a6d1041cc04d7e62938f96d1a05c52299b";
+
+/// à  continuer
